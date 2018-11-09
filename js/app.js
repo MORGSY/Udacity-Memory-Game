f902a7433ef8ab9d1f25c43a657ca6ca63812109
Morgan N <xmorgsx@gmail.com>
2018-11-08 16:11:22 -0800

    more moda changes

--- js/app.js
+++ js/app.js
@@ -155,7 +155,7 @@ function eventListener(e) { //event listener function
     }
 }
 
-//Creates the modal
+//Create modal
 let modalAppear = function() {
     if (matchList.length == 16) {
         clearInterval(startTimer);
@@ -172,7 +172,7 @@ let modalAppear = function() {
     }
 };
 
-//Closes the modal when the button is clicked
+//Closes modal when the button is clicked
 function closeModal() {
     button.addEventListener("click", function() {
         document.getElementById("modal-popup").style.display = "none";
@@ -203,14 +203,3 @@ document.addEventListener('click', initiateStopwatch);
 
 //event listener
 document.addEventListener('click', eventListener);
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
